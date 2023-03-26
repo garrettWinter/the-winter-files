f3d1df145fe7488444197298819cdd708f2b6c67
Garrett Winter <Garrett_Winter@hotmail.com>
2023-03-26 16:36:34 -0500

    Started to style and update the footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+
+import gitHub from '../../../src/assets/images/github.png'
+import linkedIn from '../../../src/assets/images/linkedin.png'
+import stackOverflow from '../../../src/assets/images/stack-overflow.png'
+
 
 const Footer = () => {
   return (
-    <footer> 
-        <div>Footer! Pulling from footer component</div>
-        <div>Contact Logos (gitHub) (LinkedIn) (Stackoverflow?)</div>
-    </footer>
+    <Container maxWidth="full" disableGutters={true}>
+      <Box sx={{
+        bgcolor: '#000000',
+      }}>
+        <footer>
+          <div>Footer! Pulling from footer component</div>
+          <div>
+            <img src={gitHub} className="tbd" alt="Link to gitHub profile" />
+            <img src={linkedIn} className="tbd" alt="Link to linkedIn profile" />
+            <img src={stackOverflow} className="tbd" alt="Link to stackOverflow profile" />
+          </div>
+        </footer>
+      </Box>
+    </Container>
   );
 };
 
