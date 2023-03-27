0fd926d76afa648e0eb9d246ba8e6da1296cff2b
Garrett Winter <Garrett_Winter@hotmail.com>
2023-03-27 15:28:11 -0500

    Added the material icon package, and continued development of the Contact Me page.

--- src/components/pages/contactMe/index.js
+++ src/components/pages/contactMe/index.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
 
 import emailValidation from '../../../utils/helpers';
 
@@ -11,12 +13,10 @@ const ContactMe = () => {
   const [errorMessage, setErrorMessage] = useState('');
 
   const handleInputChange = (e) => {
-    // Getting the value and name of the input which triggered the change
     const { target } = e;
     const inputType = target.name;
     const inputValue = target.value;
 
-    // Based on the input type, we set the state of either email, username, and password
     if (inputType === 'email') {
       setEmail(inputValue);
     } else if (inputType === 'userName') {
@@ -27,54 +27,91 @@ const ContactMe = () => {
   };
 
   const handleFormSubmit = (e) => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
     e.preventDefault();
 
-    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
-    if (!emailValidation(email) || !fullName) {
-      setErrorMessage('Email Address or Name entry is invalid');
-      // We want to exit out of this code block if something is wrong so that the user can correct it
+    if (!fullName) {
+      setErrorMessage('Please include your first and name in this message.');
       return;
-      // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
     }
-
+    if (!emailValidation(email)) {
+      setErrorMessage('Please provide a valid email address.');
+      return;
+    }
+    if (!message) {
+      setErrorMessage('Please include a message in your submission.');
+      return;
+    }
     // If everything goes according to plan, we want to clear out the input after a successful registration.
+    setErrorMessage('Your information has been submitted, thank you for contacting me!');
     setFullName('');
     setMessage('');
     setEmail('');
   };
 
+  const styles = {
+    contactBox: {
+      maxWidth: '100%'
+    },
+    formFieldDiv: {
+      padding: '3px 0px'
+    },
+    formFieldText: {
+      fontWeight: 'bold'
+    },
+    formFieldInput: {
+      maxWidth: '500px',
+      minWidth: '50%'
+    },
+    formFieldInputMessage: {
+      maxWidth: '500px',
+      minWidth: '50%',
+      height: '150px',
+    }
+  }
+
   return (
     <Container maxWidth="full" disableGutters={true}>
       <Box sx={{
         textAlign: 'left',
-        padding: '10px 10%'
+        padding: '10px 10%',
       }}>
         <h2>Contact Me</h2>
-        <div>
+        <div style={styles.contactBox}>
           <form className="form">
-            <input
-              value={fullName}
-              name="userName"
-              onChange={handleInputChange}
-              type="text"
-              placeholder="First and Last Name"
-            />
-            <input
-              value={email}
-              name="email"
-              onChange={handleInputChange}
-              type="email"
-              placeholder="Email Address"
-            />
-            <input
-              value={message}
-              name="message"
-              onChange={handleInputChange}
-              type="message"
-              placeholder="Message for Garrett"
-            />
-            <button type="button" onClick={handleFormSubmit}>Submit</button>
+            <div style={styles.formFieldDiv}>
+              <div style={styles.formFieldText}>Name:</div>
+              <input
+                style={styles.formFieldInput}
+                value={fullName}
+                name="userName"
+                onChange={handleInputChange}
+                type="text"
+                placeholder="First and Last Name"
+              />
+            </div>
+            <div style={styles.formFieldDiv}>
+              <div style={styles.formFieldText}>Email Address:</div>
+              <input
+                style={styles.formFieldInput}
+                value={email}
+                name="email"
+                onChange={handleInputChange}
+                type="email"
+                placeholder="Email Address"
+              />
+            </div>
+            <div style={styles.formFieldDiv}>
+              <div style={styles.formFieldText}>Message:</div>
+              <textarea
+                style={styles.formFieldInputMessage}
+                value={message}
+                name="message"
+                onChange={handleInputChange}
+                type="message"
+                placeholder="Message for Garrett"
+              />
+            </div>
+            <Button variant="contained" endIcon={<SendIcon />} color="success" type="button" onClick={handleFormSubmit}>Submit</Button>
           </form>
           {errorMessage && (
             <div>
