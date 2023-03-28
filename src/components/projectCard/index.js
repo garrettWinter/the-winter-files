77f454e695951f44873a91ee3409931e82b3eb68
Garrett Winter <Garrett_Winter@hotmail.com>
2023-03-28 13:38:55 -0500

    Updated height of productCards so they would be uniform height in their grid rows.

--- src/components/projectCard/index.js
+++ src/components/projectCard/index.js
@@ -8,7 +8,7 @@ import Typography from '@mui/material/Typography';
 
 export default function ProjectCard({projectDetails}) {
   return (
-    <Card sx={{ tbd: ''}}>
+    <Card sx={{ height: '100%' }}>
       <CardMedia
         component="img"
         alt={projectDetails.altText}
