1d487a5ad065ccae66eee8d4e19b30b584e08fe6
Garrett Winter <Garrett_Winter@hotmail.com>
2023-03-29 13:53:06 -0500

    updated formatting of new text

--- src/components/pages/aboutMe/index.js
+++ src/components/pages/aboutMe/index.js
@@ -24,11 +24,11 @@ const AboutMe = () => {
       }}>
         <h2>About Me</h2>
         <div><img src={garrettPicture} alt="Link to gitHub profile" style={styles.profilePic} /></div>
-        <div style={styles.bodyText}>Hello, I'm Garrett Winter, an experienced e-commerce professional and a recent graduate of the Full Stack Web Development program at the University of Minnesota.
+        <div style={styles.bodyText}>Hello, I'm Garrett Winter, an experienced e-commerce professional and a recent graduate of the Full Stack Web Development program at the University of Minnesota.<br></br>
 
-          With over 14 years of experience in e-commerce, I have a deep understanding of the industry and have spent a significant portion of my career managing international teams and providing integration monitoring and support. I've also worked as a project manager, overseeing the implementation of websites into our platform and leading the development of payment methods from our platform to be used in APIs.
+          With over 14 years of experience in e-commerce, I have a deep understanding of the industry and have spent a significant portion of my career managing international teams and providing integration monitoring and support. I've also worked as a project manager, overseeing the implementation of websites into our platform and leading the development of payment methods from our platform to be used in APIs. <br></br>
 
-          As a developer, I specialize in both front-end and back-end technologies, including HTML5, CSS, JavaScript, Bootstrap, Bulma, Material UI, HandlebarsJS, React, Node.JS, MySQL, MongoDB, REST APIs, Express, and GraphQL. My recent completion of the Full Stack Web Development program at the University of Minnesota has equipped me with the latest tools and technologies to become a well-rounded full-stack developer.
+          As a developer, I specialize in both front-end and back-end technologies, including HTML5, CSS, JavaScript, Bootstrap, Bulma, Material UI, HandlebarsJS, React, Node.JS, MySQL, MongoDB, REST APIs, Express, and GraphQL. My recent completion of the Full Stack Web Development program at the University of Minnesota has equipped me with the latest tools and technologies to become a well-rounded full-stack developer.<br></br>
 
           In my spare time, I love to stay active by running, and I also enjoy playing games. I'm excited about the opportunity to bring my skills and experience to a new role as a developer.</div>
       </Box>
